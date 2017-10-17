4bb5abd6e6bb2392e4ba73a65d1a006993792f0c
kubasok <jakubsokolowski@hotmail.com>
2017-10-17 22:07:35 +0200

    check

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-//import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
-
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 
